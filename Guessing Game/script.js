1f4a42ca80feb14aa2b8301529427ed0d43ee953
harshkchoudhary11902012 <choudharyharshofficial@gmail.com>
2024-05-03 00:04:42 +0530

    Guessing Game Initial commit

---
+++ Guessing Game/script.js
@@ -0,0 +1,40 @@
+let input = document.getElementById("input");
+let btn = document.getElementById("btn");
+let wrng = document.querySelector(".wrng");
+let guesses = document.getElementById("guesses");
+
+let answer = Math.floor(Math.random() * 100) + 1;
+console.log(answer);
+
+let numGuesses = 0;
+btn.addEventListener("click", () => {
+  guessesNumber();
+});
+function guessesNumber() {
+  if (input.value < 1 || input.value > 100 || isNaN(input.value)) {
+    wrng.innerHTML = "Please enter a number between 1 and 100";
+  } else {
+    numGuesses++;
+    guesses.innerHTML = "No. of Guess: " + numGuesses;
+
+    if (input.value > answer) {
+      wrng.innerHTML = "Your guess is too high";
+      input.value = "";
+    } else if (input.value < answer) {
+      wrng.innerHTML = "Your guess is too low";
+      input.value = "";
+    } else {
+      wrng.innerHTML = "Congratulations !!!! You guessed the correct answer";
+      setTimeout(() => {
+        resetGame();
+      }, 5000);
+    }
+  }
+}
+
+function resetGame() {
+  numGuesses = 0;
+  answer = Math.floor(Math.random() * 100) + 1;
+  input.value = "";
+  guesses.innerHTML = "No. of Guess: 0";
+}
